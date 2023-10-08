d7be65c3849e78d810a91a4db65d71a5e0467f55
mlaane <goobric@gmail.com>
2023-10-08 15:45:58 +0100

    all file

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// check if number is a palindrome when reversed
+
+using System;
+
+class Program
+{
+  static bool IsPalindrome(int number)
+  {
+    int originalNumber = number;
+    int reversedNumber = 0;
+
+    while (number > 0)
+    {
+      int digit = number % 10;
+      reversedNumber = reversedNumber * 10 + digit;
+      number /= 10;
+    }
+    return originalNumber == reversedNumber;
+  }
+
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Enter a number: ");
+    int number = int.Parse(Console.ReadLine());
+
+    if (IsPalindrome(number))
+    {
+      Console.WriteLine($"{number} is a Palindrome");
+    }
+    else
+    {
+      Console.WriteLine($"{number} is not a palindrome :-( ");
+    }
+  }
+}
